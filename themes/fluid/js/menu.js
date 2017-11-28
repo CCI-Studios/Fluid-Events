a5324204f2b390b7857bffd1382a795d6ce3db85
Chris Morris <cmorris@ccistudios.com>
2017-11-28 13:51:22 -0500

    white or color fixed header depending on the background color

--- themes/fluid/js/menu.js
+++ themes/fluid/js/menu.js
@@ -1,15 +1,66 @@
 (function($){
+  var blockPositions = [];
+
   $(function(){
     $(".show-menu").on("click", showMenu);
     $(".hide-menu").on("click", hideMenu);
+    $(window).on("load resize", resize);
     $(window).on("scroll", scroll);
+    resize();
     scroll();
   });
+  function resize() {
+    blockPositions = [];
+    $(".block--custom-page-title, "+
+      ".paragraph--type--content-with-background, "+
+      ".block--splash-logo, "+
+      ".block--collapsible-image-content, "+
+      ".block--image-content"
+    )
+    .each(function(){
+      blockPositions.push({
+        top: $(this).position().top-75,
+        isDark: true
+      });
+    });
+    $(".block--main, "+
+      ".paragraph--type--basic-content, "+
+      ".paragraph--type--image-content"
+    )
+    .each(function(){
+      blockPositions.push({
+        top: $(this).position().top-75,
+        isDark: false
+      });
+    });
+    blockPositions.sort(function(a,b){
+      return a.top - b.top;
+    });
+  }
   function scroll() {
-    if ($(window).scrollTop() >= 120) {
-      $("body").addClass("show-logo");
-    } else {
+    var scrollTop = $(window).scrollTop();
+    //show/hide header logo
+    if ($("body").hasClass("path-frontpage") && scrollTop <= 120) {
       $("body").removeClass("show-logo");
+    } else {
+      $("body").addClass("show-logo");
+    }
+
+    //show the white or colour logo & main menu
+    if (blockPositions.length < 2) {
+      return;
+    }
+    for (var i=blockPositions.length-1; i >= 0 ; i--) {
+      if (scrollTop >= blockPositions[i].top && (i == blockPositions.length-1 || scrollTop <= blockPositions[i+1].top)) {
+        if (blockPositions[i].isDark) {
+          $("body").addClass("bg-dark");
+        } else {
+          $("body").removeClass("bg-dark");
+        }
+        break;
+      } else {
+        continue;
+      }
     }
   }
   function showMenu() {
