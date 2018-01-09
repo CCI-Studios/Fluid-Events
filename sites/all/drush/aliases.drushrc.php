1d7140eb95be9f0bbfcd7c761da884eae9bde51d
Chris Morris <cmorris@ccistudios.com>
2018-01-09 09:59:16 -0500

    added drush alias for staging site

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['staging'] = array(
+	'uri'=> 'fluidevents.ccistaging.com',
+	'root' => '/home/staging/subdomains/fluidevents/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'staging',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+);
