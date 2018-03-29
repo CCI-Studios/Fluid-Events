f914dd174e31d3dcbb9025704f214998fc9a39e6
Chris Morris <cmorris@ccistudios.com>
2018-03-29 09:28:27 -0400

    added live site to drush aliases

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -9,3 +9,13 @@ $aliases['staging'] = array(
 		'%files'=> 'sites/default/files',
 	),
 );
+
+$aliases['live'] = array(
+	'uri'=> 'www.fluidevents.ca',
+	'root' => '/home/fluid/subdomains/live/public_html',
+	'remote-host'=> 'host.cciserver2.com',
+	'remote-user'=> 'fluid',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+);
